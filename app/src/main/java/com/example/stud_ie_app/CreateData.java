064303a084f9dcb961ef268f362065da7581ee42
Kevin Nguyen <kevinngx625@gmail.com>
2019-04-17 17:32:43 +1000

    Started UI

---
+++ app/src/main/java/com/example/stud_ie_app/CreateData.java
@@ -0,0 +1,29 @@
+package com.example.stud_ie_app;
+
+public class CreateData {
+
+    public void populateUser() {
+        //TODO: Create array list of database objects
+        //TODO: Add data to array list
+        //TODO: Add arraylist into insertAll Statement
+    }
+
+    public void populateUserBadges() {
+        //TODO: Create array list of database objects
+        //TODO: Add data to array list
+        //TODO: Add arraylist into insertAll Statement
+    }
+
+    public void populateBadges() {
+        //TODO: Create array list of database objects
+        //TODO: Add data to array list
+        //TODO: Add arraylist into insertAll Statement
+    }
+
+    public void populateWord() {
+        //TODO: Create array list of database objects
+        //TODO: Add data to array list
+        //TODO: Add arraylist into insertAll Statement
+    }
+
+}
--- app/src/main/java/com/example/stud_ie_app/MainActivity.java
+++ app/src/main/java/com/example/stud_ie_app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.stud_ie_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,26 +21,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void onButtonPress(View view) throws ExecutionException, InterruptedException {
 
-        TextView wordText = findViewById(R.id.wordText);
-        TextView sentenceView = findViewById(R.id.sentenceText);
+//        TextView wordText = findViewById(R.id.wordText);
+//        TextView sentenceView = findViewById(R.id.sentenceText);
+//
+//        WordApi wordApi = WordHelper.getWord();
+//        OxfordDictionaryApi oxfordDictionaryApi = SentenceHelper.getSentence(wordApi.getWord());
+//
+//        while (oxfordDictionaryApi == null) {
+//            wordApi = WordHelper.getWord();
+//            oxfordDictionaryApi = SentenceHelper.getSentence(wordApi.getWord());
+//        }
+//
+//        wordText.setText(wordApi.getWord());
+//        String sentence = oxfordDictionaryApi.getSentencesList().get(0).text;
+//        System.out.println(sentence);
+//        sentenceView.setText(sentence);
 
-        WordApi wordApi = WordHelper.getWord();
-        OxfordDictionaryApi oxfordDictionaryApi = SentenceHelper.getSentence(wordApi.getWord());
-
-        while (oxfordDictionaryApi == null) {
-            System.out.println("Null result, searching again");
-            wordApi = WordHelper.getWord();
-            oxfordDictionaryApi = SentenceHelper.getSentence(wordApi.getWord());
-        }
-
-        System.out.println("Result is found!");
-
-        wordText.setText(wordApi.getWord());
-        String sentence = oxfordDictionaryApi.getSentencesList().get(0).text;
-        System.out.println(sentence);
-        sentenceView.setText(sentence);
-
-
+        Intent intent = new Intent(this, LevelSelectActivity.class);
+        startActivity(intent);
 
     }
 
