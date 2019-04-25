4d9fdd26e34b6565a0d421a2b7010aaccc758e50
Muhammad <muhammad.jaame@gmail.com>
2019-04-26 02:06:01 +1000

    badge composite made

---
+++ app/src/main/java/com/example/stud_ie_app/UsrBadges.java
@@ -0,0 +1,46 @@
+package com.example.stud_ie_app;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+
+@Entity
+public class UsrBadges {
+    @NonNull
+    @PrimaryKey
+    private String UsrBadgeID;
+    private String username;
+    private int BadgeID;
+
+    public UsrBadges(String usrBadgeID, String username, int badgeID) {
+        this.UsrBadgeID = username + badgeID;
+        this.username = username;
+        this.BadgeID = badgeID;
+    }
+
+    @NonNull
+    public String getUsrBadgeID() {
+        return UsrBadgeID;
+    }
+
+    public void setUsrBadgeID(@NonNull String usrBadgeID) {
+        UsrBadgeID = usrBadgeID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getBadgeID() {
+        return BadgeID;
+    }
+
+    public void setBadgeID(int badgeID) {
+        BadgeID = badgeID;
+    }
+}
